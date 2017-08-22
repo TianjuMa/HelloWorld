e4146dce9a42ce5a9191e2ab063a034683bf705f
jasonMatj <matianju@gmail.com>
2017-08-22 16:06:09 -0500

    EvenIterator update

--- src/LaiOffer/MyIterators/EvenIterator.java
+++ src/LaiOffer/MyIterators/EvenIterator.java
@@ -1,16 +1,19 @@
 package LaiOffer.MyIterators;
 
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by matinaju on 6/17/17.
  */
 public class EvenIterator {
+    List<Integer> list;
     Iterator<Integer> ite;
     Integer temp;
 
-    public EvenIterator(Iterator<Integer> inputIte) {
-        this.ite = inputIte;
+    public EvenIterator(List<Integer> input) {
+        this.list = input;
+        this.ite = input.iterator();
         temp = null;
     }
 
@@ -19,12 +22,13 @@ public class EvenIterator {
             return true;
         }
         for (int i = 0; i < 2; i++) {
-            if (!hasNext()) {
+            if (ite.hasNext()) {
+                temp = ite.next();
+            } else {
                 return false;
             }
-            temp = ite.next();
         }
-        return false;
+        return true;
     }
 
     public int next() {
